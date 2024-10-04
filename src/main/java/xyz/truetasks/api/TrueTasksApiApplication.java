03d162e17cabf3b7b2becd881aa494e8fbd6a488
Eric Freitas <freitaseric@proton.me>
2024-10-03 21:43:21 -0400

    chore: initial commit

---
+++ src/main/java/xyz/truetasks/api/TrueTasksApiApplication.java
@@ -0,0 +1,13 @@
+package xyz.truetasks.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrueTasksApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrueTasksApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/truetasks/api/TrueTasksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.truetasks.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrueTasksApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
